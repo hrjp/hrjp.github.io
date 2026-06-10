df22ad4bddadd645a6a9dc592ea99a1d6af07860
Shunya Hara <r115159@g.kobe-kosen.ac.jp>
2026-06-10 23:28:22 +0900

    メディア見出しのテキストを「メディ」に更新

--- assets/main.js
+++ assets/main.js
@@ -62,7 +62,7 @@ const UI_TEXT = {
       nonpeer: "査読無し",
       award: "受賞"
     },
-    mediaHeading: "メディア掲載",
+    mediaHeading: "メディア",
     mediaEmpty: "テレビ・書籍などの掲載歴を追加できます。",
     mediaKinds: {
       tv: "テレビ",
